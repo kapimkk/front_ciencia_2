633394bd9d3f2aa34d393311bb2020f6b7c27112
kapimkk <cappellifelipe@gmail.com>
2025-10-23 00:51:18 +0000

    Turista

---
+++ aulas/javascript/operadores.js
@@ -0,0 +1,40 @@
+/*Operadores Aritiméticos*/
+// + - * / % **
+
+/*Outros Operadores*/
+// > < <= >= != (maior que, menor que, menor ou igual, maior ou igual, diferente)
+// && || (E Lógico, OU Lógico)
+/*
+const isPar = 15 // Number
+
+if (isPar % 2 == 0) {
+  // Trata a quando a condição for verdadeira
+  alert('O numero ' + isPar + ' é par')
+} else {
+  // Trata a condição quando for falsa
+  alert('o numero ' + isPar + ' é impar')
+}
+
+
+console.log(isPar % 2 == 0)
+*/
+// Pessoas com Idade entre 18 a 70 anos podem entrar
+// pessoas maiores de 70 precisam de companhia
+// Menores Vazam
+/*
+const idade = prompt('Digite sua Idade: ')
+if (isNaN(idade)) {
+  alert('Digite uma Idade Válida, Coloque Um Número')
+} else if (idade >= 0 && idade <= 122) {
+  if (idade >= 18 && idade <= 70) {
+    alert('Você tem ' + idade + ' anos \nPode Entrar no Baile')
+  } else if (idade < 18) {
+    alert('Vaza Menor Problema')
+  } else if (idade > 70) {
+    alert('Voce Precisa de Um Acompanhante')
+  }
+} else {
+  alert('Digite uma Idade Válida')
+}
+*/
+
---
+++ aulas/turista/app.js
@@ -0,0 +1,18 @@
+const lugares = ["montanha", "praia"]
+const mensagem = `
+    ---Roteiro de Viagens---
+    Lugares: ${lugares}
+    ---------------------------`
+alert(mensagem)
+const escolhaUsuario = prompt('Pra onde deseja viajar: ')
+
+if (!escolhaUsuario) {
+  alert("Por favor digite um valor da lista de viagens.")
+} else {
+  for (const lugar of lugares) {
+    if (escolhaUsuario.toLowerCase() == lugar) {
+      // alert(lugar)
+      window.location.href = `${lugar}.html`
+    }
+  }
+}
